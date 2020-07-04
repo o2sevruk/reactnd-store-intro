a225ba565e4d7a4bc1e96dfb101dc70c3cf0f866
o2sevruk <o2sevruk@gmail.com>
2020-07-04 15:54:31 +0300

    type: add functionality description: "goals" reducer added

--- index.js
+++ index.js
@@ -32,11 +32,28 @@ function createState(reducer) {
 }
 
 function todos(state = [], action) {
-	if (action.type === 'ADD_TODO') {
-		return state.concat([action.todo]);
+	switch (action.type) {
+		case 'ADD_TODO' :
+			return state.concat([action.todo]);
+		case 'REMOVE_TODO' :
+			return state.filter((todo) => todo.id !== action.id);
+		case 'TOGGLE_TODO' :
+			return state.map((todo) => todo.id !== action.id ? state :
+				Object.assign({}, todo, {complete: !todo.complete}));
+		default:
+			return state;
 	}
-	
-	return state;
+}
+
+function goals(state = [], action) {
+	switch (action.type) {
+		case 'ADD_GOAL' :
+			return state.concat([action.goal]);
+		case 'REMOVE_GOAL' :
+			return state.filter((goal) => goal.id !== action.id);
+		default :
+			return state;
+	}
 }
 
 const store = createState(todos);
